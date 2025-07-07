ebb13723345a96e4eb20a75bfc263b7249fb67b8
Eduardo Luz <eslpro98@hotmail.com>
2025-07-07 16:17:45 -0300

    adicionado componente p_img para definição de parágrafos com imagens

---
+++ resources/views/components/p_img.blade.php
@@ -0,0 +1,17 @@
+@props(['img_side' => 'right', 'template', 'img_path'])
+
+@if ($img_side == 'right')
+    <div {{ $attributes->merge(['class' => 'grid gap-8 md:grid-cols-2 items-center px-4 md:px-20 lg:px-52']) }}>
+        <p class="paragraph-light">
+            {{ $slot }}
+        </p>
+        <img src="{{ asset('assets/templates/' . $template . $img_path) }}" alt="">
+    </div>
+@else
+    <div {{ $attributes->merge(['class' => 'grid gap-8 md:grid-cols-2 items-center px-4 md:px-20 lg:px-52']) }}>
+        <img src="{{ asset('assets/templates/' . $template . $img_path) }}" alt="">
+        <p class="paragraph-light">
+            {{ $slot }}
+        </p>
+    </div>
+@endif
--- resources/views/templates/vertex-one/sites.blade.php
+++ resources/views/templates/vertex-one/sites.blade.php
@@ -4,38 +4,30 @@
     <section class="w-full flex flex-col pt-20">
         <x-title size="sm-latin" emphasis_first emphasis="Criação" main="de Sites"></x-title>
         <div class="py-8">
-            <div class="grid gap-8 md:grid-cols-2 items-center px-4 md:px-20 lg:px-52">
-                <p class="paragraph-light">
-                    Hoje em dia, construir um site se tornou algo simples. A ampla oferta de ferramentas disponíveis permite
-                    que, mesmo com pouca experiência, qualquer pessoa consiga criar um. Mas construir um site que realmente
-                    atenda às necessidades do seu projeto, do seu negócio ou dos seus usuários exige uma estratégia sólida.
-                </p>
-                <img src="{{ asset('assets/templates/' . $template . '/svg/computer.svg') }}" alt="">
-            </div>
-            <div class="grid gap-8 md:grid-cols-2 items-center bg-vertexBlue text-white px-4 md:px-20 lg:px-52">
-                <img src="{{ asset('assets/templates/' . $template . '/svg/question_mark.svg') }}" alt="">
-                <p class="paragraph-light">
-                    Construir pode ser mais fácil, mas responder perguntas essenciais para definir a estratégia correta, nem
-                    tanto.<br><br>
-                    Por exemplo: quais são seus objetivos? Seu site será institucional, e-commerce, portal de conteúdo ou
-                    algo diferente? Você quer gerar leads, vender ou apenas informar? Quais KPIs (indicadores-chave de
-                    performance) serão usados? Quem é seu público-alvo? Em quais dispositivos eles acessam?<br><br>
-                    Essas são apenas algumas das muitas perguntas que precisam de resposta antes mesmo de começar a
-                    construção.
-                </p>
-            </div>
-            <div class="grid gap-8 md:grid-cols-2 items-center px-4 md:px-20 lg:px-52">
-                <p class="paragraph-light">
-                    Com a experiência da Vertex, você terá apoio para definir uma estratégia clara para seu
-                    projeto. Com a estratétgia definida, se quiser, pode até construir o site por conta própria. Ou então
-                    pode deixar tudo com a gente. Seja como for, o contato com você será constante, da maneira que for mais
-                    confortável. Não gosta de videoconferências? Fazemos reuniões presenciais. Prefere receber um resumo
-                    diário do andamento? Enviamos
-                    no ritmo que você preferir, pelo meio que você mais usa (e-mail, WhatsApp, etc.). Tudo isso para que
-                    você esteja sempre por dentro de tudo o que está acontecendo com seu projeto.
-                </p>
-                <img src="{{ asset('assets/templates/' . $template . '/img/webdesign_tools.jpg') }}" alt="">
-            </div>
+            <x-p_img :template="$template" img_path="/svg/computer.svg">
+                Hoje em dia, construir um site se tornou algo relativamente simples. A ampla oferta de ferramentas
+                disponíveis permite que, mesmo com pouca experiência, qualquer pessoa consiga
+                criar um. Mas construir um site que realmente atenda às necessidades do seu projeto, do seu negócio ou
+                dos seus usuários exige uma estratégia sólida.
+            </x-p_img>
+            <x-p_img img_side="left" :template="$template" img_path="/svg/question_mark.svg" class="bg-vertexBlue text-white">
+                Construir pode ser mais fácil, mas responder perguntas essenciais para definir a estratégia correta, nem
+                tanto.<br><br>
+                Por exemplo: quais são seus objetivos? Seu site será institucional, e-commerce, portal de conteúdo ou
+                algo diferente? Você quer gerar leads, vender ou apenas informar? Quais KPIs (indicadores-chave de
+                performance) serão usados? Quem é seu público-alvo? Em quais dispositivos eles acessam?<br><br>
+                Essas são apenas algumas das muitas perguntas que precisam de resposta antes mesmo de começar a
+                construção.
+            </x-p_img>
+            <x-p_img :template="$template" img_path="/img/webdesign_tools.jpg">
+                Com a experiência da Vertex, você terá apoio para definir uma estratégia clara para seu
+                projeto. Com a estratétgia definida, se quiser, pode até construir o site por conta própria. Ou então
+                pode deixar tudo com a gente. Seja como for, o contato com você será constante, da maneira que for mais
+                confortável. Não gosta de videoconferências? Fazemos reuniões presenciais. Prefere receber um resumo
+                diário do andamento? Enviamos
+                no ritmo que você preferir, pelo meio que você mais usa (e-mail, WhatsApp, etc.). Tudo isso para que
+                você esteja sempre por dentro de tudo o que está acontecendo com seu projeto.
+            </x-p_img>
         </div>
     </section>
 @endsection
